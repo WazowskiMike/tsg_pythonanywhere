1da423555e6a3b7c37c69acf89189eddbcd985d7
WazowskiMike <wazowskipuffs@yandex.ru>
2024-03-14 19:51:20 +0300

    tried to fix bg

--- tsg_website/views.py
+++ tsg_website/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'tsg_website/index.html')
+
+def catalog(request):
+    return render(request, 'tsg_website/catalog.html')
