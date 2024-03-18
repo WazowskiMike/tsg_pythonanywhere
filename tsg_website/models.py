146584d203a184d8bed07cd1454a5d675681401b
wazowskipuffs <wazowskipuffs@yandex.ru>
2024-03-18 20:13:44 +0000

    added SOLD func

--- tsg_website/models.py
+++ tsg_website/models.py
@@ -6,10 +6,11 @@ class Car(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=0)
     formatted_price = models.CharField(max_length=20, blank=True)
+    is_sold = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
-    
+
     def save(self, *args, **kwargs):
         self.formatted_price = intcomma(self.price)
         super().save(*args, **kwargs)
