c5facf92e3d06ce9fa12aad6bdedd17c38725212
wazowskipuffs <you@example.com>
2024-03-22 14:24:11 +0300

    added search

--- tsg_website/urls.py
+++ tsg_website/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    path('catalog/', views.catalog)
+    path('catalog/', views.catalog, name='catalog'),
 ]
--- tsg_website/views.py
+++ tsg_website/views.py
@@ -7,8 +7,12 @@ def index(request):
 def catalog(request):
     cars = Car.objects.all()
     user = request.user
+    query = request.GET.get('search')  # Получаем значение поискового запроса
+    if query:  # Если запрос присутствует, фильтруем список машин
+        cars = cars.filter(name__icontains=query)
     context = {
-        'cars':cars,
-        'user':user
+        'cars': cars,
+        'user': user,
+        'query': query  # Передаем значение запроса в контекст для отображения в шаблоне
     }
     return render(request, 'tsg_website/catalog.html', context=context)
